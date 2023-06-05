e2abffce41ea05df54d1cff63e60aa323f9222d8
brunogsr <brunogsr https://github.com/brunogsr>
2023-06-05 16:50:21 -0300

    Enviando provider, Login ainda em processo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div>Login.</div>
+    <form>
+      <label htmlFor="email">
+        <input
+          id="email"
+          name="email"
+          value={ email }
+          onChange={ ({ target }) => setEmail(target.value) }
+          data-testid="email-input"
+        />
+      </label>
+      <label htmlFor="password">
+        <input
+          type="password"
+          name="password"
+          value={ password }
+          onChange={ ({ target }) => setPassword(target.value) }
+          id="password"
+          data-testid="password-input"
+        />
+      </label>
+      <label htmlFor="submit">
+        <button
+          name="submit"
+          id="submit"
+          data-testid="login-submit-btn"
+        >
+          submit
+        </button>
+      </label>
+    </form>
   );
 }
 
