a9efb1aec29c41fac5b20cdaffc765b3dd6117f8
brunogsr <brunogsr https://github.com/brunogsr>
2023-06-05 19:50:00 -0300

    Tela de login e testes feitos

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,17 +1,37 @@
-// import { render, screen, waitFor } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import { renderWithRouterAndRedux } from './helpers/renderWith';
-// import Login from '../pages/Login';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+// import { act } from 'react-dom/test-utils';
+import Login from '../pages/Login';
 
-// describe('Testando Login Page', () => {
-//   renderWithRouterAndRedux(<Login />);
-//   it('Verifica se o botão de login está desabilitado e se habilita conforme os inputs são validados', () => {
-//     const loginButton = screen.getByTestId('login-submit-btn');
-//     const emailInput = screen.getByTestId('email-input');
-//     const passwordInput = screen.getByTestId('password-input');
-//     expect(loginButton).toBeDisabled();
-//     userEvent.type(emailInput, 'alguem@alguem.com');
-//     userEvent.type(passwordInput, '1234567');
-//     expect(loginButton).not.toBeDisabled();
-//   });
-// });
+describe('Testando Login Page', () => {
+  const validEmail = 'alguem@alguem.com';
+  it('Verifica se o botão de login está desabilitado e se habilita conforme os inputs são validados', () => {
+    render(<Login />);
+    const loginButton = screen.getByTestId('login-submit-btn');
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    expect(loginButton).toBeDisabled();
+    userEvent.type(emailInput, validEmail);
+    userEvent.type(passwordInput, '1234567');
+    expect(loginButton).not.toBeDisabled();
+  });
+  it('Verifica se ao clicar no botão de login, o localStorage é atualizado com o email digitado', async () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <Login />
+      </Router>,
+    );
+    const email = 'alguem@alguem.com';
+    const loginButton = screen.getByTestId('login-submit-btn');
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    userEvent.type(emailInput, email);
+    userEvent.type(passwordInput, '1234567');
+    userEvent.click(loginButton);
+    await waitFor(() => expect(history.location.pathname).toBe('/meals'));
+    expect(localStorage.getItem('user')).toBe(JSON.stringify({ email }));
+  });
+});
