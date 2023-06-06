b5ca1bad3f45206831355c47523ef3842bdfb416
lucastelesss <lucastelessouza005@gmail.com>
2023-06-06 16:12:44 -0300

    iniciando requisito 19 - tela principal de receitas

--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 
 function Meals() {
+  useEffect(() => {
+    try{
+      const fetchMeals = async () => {
+        await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
+      }
+    }
+
+  }, []);
+
   return (
     <div>
       <Header title="Meals" />
