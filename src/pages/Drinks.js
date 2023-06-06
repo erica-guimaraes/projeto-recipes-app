c1c65d0c454493d61a8c9224bc4eb296a95543ae
lucastelesss <lucastelessouza005@gmail.com>
2023-06-06 18:30:30 -0300

    requisito - 19 - carrega 12 primeiras receitas de comidas ou bebidas, uma em cada card

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CardRecipe from '../components/CardRecipe';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 
 function Drinks() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchDrinks = async () => {
+      const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+      const data = await response.json();
+      const { drinks } = data;
+      const firstTwelveRecipes = 12;
+      setRecipes(drinks.slice(0, firstTwelveRecipes));
+    };
+
+    fetchDrinks();
+  }, []);
+
+  console.log(recipes);
+
   return (
     <div>
       <Header title="Drinks" />
       <SearchBar endpoint="https://www.thecocktaildb.com/api/json/v1/1/" screen="Drinks" />
       <Footer />
+      <ul>
+        {recipes.map((recipe, index) => (
+          <CardRecipe recipe={ recipe } index={ index } key={ recipe.idDrink } />
+        ))}
+      </ul>
     </div>
   );
 }
--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,23 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import CardRecipe from '../components/CardRecipe';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 
 function Meals() {
-  useEffect(() => {
-    try{
-      const fetchMeals = async () => {
-        await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
-      }
-    }
+  const [recipes, setRecipes] = useState([]);
 
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+      const data = await response.json();
+      const { meals } = data;
+      const firstTwelveRecipes = 12;
+      setRecipes(meals.slice(0, firstTwelveRecipes));
+    };
+
+    fetchMeals();
   }, []);
+
+  console.log(recipes);
 
   return (
     <div>
       <Header title="Meals" />
       <SearchBar endpoint="https://www.themealdb.com/api/json/v1/1/" screen="Meals" />
       <Footer />
+      <ul>
+        {recipes.map((recipe, index) => (
+          <CardRecipe recipe={ recipe } index={ index } key={ recipe.idMeal } />
+        ))}
+      </ul>
     </div>
   );
 }
