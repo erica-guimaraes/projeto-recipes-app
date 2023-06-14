4b9b6545951cf43d1109884a5ae65d117d7d3d41
brunogsr <brunogsr https://github.com/brunogsr>
2023-06-14 03:34:04 -0300

    req 25 quase feito, faltando colocar vídeo do youtube nos recipedetails

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 import DetailsContext from '../context/DetailsContext';
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 function RecipeDetails() {
   const { fetchDetailsById, fetchRecipeById, loading } = useContext(DetailsContext);
@@ -13,14 +14,50 @@ function RecipeDetails() {
   }, [location]);
 
   // console.log(fetchRecipeById)
-
+  const magic10 = 10;
+  const magic13 = 13;
   return loading ? (
     <p>Loading...</p>
   ) : (
     <div>
       {fetchRecipeById && fetchRecipeById.length > 0
        && fetchRecipeById.map((option, index) => (
-         <p key={ index }>{option.strDrink || option.strMeal}</p>
+         <div key={ index }>
+           <Header title={ option.strDrink || option.strMeal } />
+           <img
+             src={ option.strDrinkThumb || option.strMealThumb }
+             alt={ option.strDrink || option.strMeal }
+             data-testid="recipe-photo"
+           />
+           <p data-testid="recipe-title">{option.strDrink || option.strMeal}</p>
+           <p data-testid="recipe-category">
+             {option.strAlcoholic
+               || option.strCategory}
+           </p>
+           <ul>
+             {Object.entries(option).map((entry, indexEntries) => { // entries para pegar o valor sem chamar fetchRecipeById novamente
+               if (
+                 entry[0].startsWith('strIngredient') // keys estavam diferentes, buscando semelhantes
+                   && entry[1] !== null && entry[1] !== '' // removendo ingredientes vazios
+               ) {
+                 const ingredientIndex = indexEntries + 1 - magic10; // gambiarra para pegar o index do ingrediente
+                 return (
+                   <li
+                     key={ entry[1] }
+                     data-testid={ `${ingredientIndex}-ingredient-name-and-measure` }
+                   >
+                     {entry[1]}
+                     {' '}
+                     {option[`strMeasure${entry[0].slice(magic13)}`]}
+                   </li>
+                 );
+               }
+               return null;
+             })}
+           </ul>
+           <p data-testid="instructions">{option.strInstructions}</p>
+         </div>
+
        ))}
       <Footer />
     </div>
