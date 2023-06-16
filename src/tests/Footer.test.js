7a662abbbc9251f5d17d2ee8036477b9924dc7b2
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-06-16 16:54:54 -0300

    Arrumando erros de lint

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -19,38 +19,38 @@ describe('Testando o componente "Footer"', () => {
     expect(drinksButton).toBeInTheDocument();
     expect(mealsButton).toBeInTheDocument();
   });
-
-//   it('Verifica se ao clicar na imagem do botão de drinks, é redirecionado para a página correta', async () => {
-//     const history = createMemoryHistory();
-//     history.push('/meals');
-//     render(
-//       <Router history={ history }>
-//         <App />
-//       </Router>,
-//     );
-//     const drinksButton = screen.getByTestId('drinks-bottom-btn');
-
-//     userEvent.click(drinksButton);
-
-//     await waitFor(() => {
-//       expect(history.location.pathname).toBe('/meals');
-//     });
-//   });
-
-//   it('Verifica se ao clicar na imagem do botão de pesquisa, é redirecionado para a página correta', async () => {
-//     const history = createMemoryHistory();
-//     history.push('/meals');
-//     render(
-//       <Router history={ history }>
-//         <App />
-//       </Router>,
-//     );
-//     const mealsButton = screen.getByTestId('meals-bottom-btn');
-
-//     userEvent.click(mealsButton);
-
-//     await waitFor(() => {
-//       expect(history.location.pathname).toBe('/meals');
-//     });
-//   });
 });
+
+// //   it('Verifica se ao clicar na imagem do botão de drinks, é redirecionado para a página correta', async () => {
+// //     const history = createMemoryHistory();
+// //     history.push('/meals');
+// //     render(
+// //       <Router history={ history }>
+// //         <App />
+// //       </Router>,
+// //     );
+// //     const drinksButton = screen.getByTestId('drinks-bottom-btn');
+
+// //     userEvent.click(drinksButton);
+
+// //     await waitFor(() => {
+// //       expect(history.location.pathname).toBe('/meals');
+// //     });
+// //   });
+
+// //   it('Verifica se ao clicar na imagem do botão de pesquisa, é redirecionado para a página correta', async () => {
+// //     const history = createMemoryHistory();
+// //     history.push('/meals');
+// //     render(
+// //       <Router history={ history }>
+// //         <App />
+// //       </Router>,
+// //     );
+// //     const mealsButton = screen.getByTestId('meals-bottom-btn');
+
+// //     userEvent.click(mealsButton);
+
+// //     await waitFor(() => {
+// //       expect(history.location.pathname).toBe('/meals');
+// //     });
+// //   });
